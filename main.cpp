e1b598aced34418458b319d1ba5daaafba99cbdd
haydenw-uk <19262752@brookes.ac.uk>
2024-10-18 23:23:11 +0100

    Add main file with default template, update gitignore for CLion

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Hayden Williams on 18/10/2024.
+//
+
+#include <iostream>
+
+// Entry point of the program
+int main() {
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
